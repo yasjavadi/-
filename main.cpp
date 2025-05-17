9ca2a12a7c2bd7a44f9f545614e75c42ee2bc4a8
yasjavadi <zahra6161.javadi@gmail.com>
2025-05-17 00:30:06 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream> //   عددی رو از ورودی دریافت کنید وتشخیص دهید منفی هست یا مثبت
+
+using namespace std;
+
+int main()
+{
+  int number;
+cout<<"enter a number:";
+cin>> number;
+  if (number>0){
+    cout<<"number is positive";
+  }
+  else {
+
+    cout<<"number is negetive";
+  }
+
+
+
+    return 0;
+}
